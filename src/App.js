c7e5705b95e85f43562304d739ba74011684d7b7
Rafael <rafael22fs@gmail.com>
2019-09-11 16:45:50 -0300

    adnt

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'antd/es/button';
+import LayoutInicial from './componentes/LayoutInicial';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Button type="primary">Button</Button>
+     <LayoutInicial/>
+    </div>
+  );
+}
+
+export default App;
